8949c32cc81eca293b8d2dc81548abbe4f03c758
Benjamin Jubb <bjubb@vividsolutions.com>
2020-01-07 12:21:18 -0800

    gruntfile fix

--- gruntfile.js
+++ gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function( grunt ) {
                     base:       '.',
                     keepalive:  true,
                     debug:      true,
-                    open:       true
+                    // open:       true
                 }
             }
         },
