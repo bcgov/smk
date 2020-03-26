0a254915080517365c5ce9f342756d7ebeafc7cf
Benjamin Jubb <benjubb@gmail.com>
2020-03-25 17:14:35 -0700

    dont show search-location for wf

--- src/smk/tool/search/check/check-search.js
+++ src/smk/tool/search/check/check-search.js
@@ -2,6 +2,10 @@ include.module( 'check-search', [], function ( inc ) {
     "use strict";
 
     return function ( smk, tool ) {
+        if ( smk.tools.some( function ( t ) { 
+            return t.type == 'search-location'
+        } ) ) return
+        
         smk.tools.push( Object.assign( {}, tool, {
             id: 'search-location',
             type: 'search-location',
