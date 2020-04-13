870b87fff3731d8491525f850d4b9f22368b34cf
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 13:32:49 -0700

    fix bespoke

--- src/smk/tool/bespoke/tool-bespoke.js
+++ src/smk/tool/bespoke/tool-bespoke.js
@@ -1,4 +1,10 @@
-include.module( 'tool-bespoke', [ 'tool.tool-panel-js', 'tool-bespoke.panel-bespoke-html', 'vue-config' ], function ( inc ) {
+include.module( 'tool-bespoke', [ 
+    'tool.tool-base-js', 
+    'tool.tool-widget-js', 
+    'tool.tool-panel-js', 
+    'tool-bespoke.panel-bespoke-html', 
+    'vue-config' 
+], function ( inc ) {
     "use strict";
 
     Vue.component( 'bespoke-widget', {
@@ -12,64 +18,54 @@ include.module( 'tool-bespoke', [ 'tool.tool-panel-js', 'tool-bespoke.panel-besp
     } )
     // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     //
-    function BespokeTool() {
-        SMK.TYPE.ToolPanel.prototype.constructor.call( this, 'bespoke-panel', 'bespoke-widget' )
-
-        this.toolProp( 'content', { 
-            forWidget: false 
-        } )
-        this.toolProp( 'component', { 
-            forWidget: false 
-        } )
-    }
-
-    SMK.TYPE.BespokeTool = BespokeTool
-
-    $.extend( BespokeTool.prototype, SMK.TYPE.ToolPanel.prototype )
-    // BespokeTool.prototype.afterInitialize = SMK.TYPE.ToolPanel.prototype.afterInitialize.concat( [] )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    BespokeTool.prototype.afterInitialize = SMK.TYPE.ToolPanel.prototype.afterInitialize.concat( function ( smk ) {
-        var self = this
+    return SMK.TYPE.Tool.define( 'BespokeTool', 
+        function () {
+            SMK.TYPE.ToolWidget.call( this, 'bespoke-widget' )
+            SMK.TYPE.ToolPanel.call( this, 'bespoke-panel' )
         
-        smk.on( this.id, {
-            'activate': function () {
-                if ( !self.enabled ) return
-
-                if ( SMK.HANDLER.has( self.id, 'triggered' ) ) {
-                    self.active = false
-                    SMK.HANDLER.get( self.id, 'triggered' )( smk, self )
+            this.defineProp( 'content' )
+            this.defineProp( 'component' )
+        },
+        function ( smk ) {
+            var self = this
+        
+            smk.on( this.id, {
+                'activate': function () {
+                    if ( !self.enabled ) return
+    
+                    if ( SMK.HANDLER.has( self.id, 'triggered' ) ) {
+                        self.active = false
+                        SMK.HANDLER.get( self.id, 'triggered' )( smk, self )
+                    }
+                },
+    
+                'swipe-up': function ( ev ) {                
+                    smk.$sidepanel.setExpand( 2 )
+                },
+    
+                'swipe-down': function ( ev ) {
+                    smk.$sidepanel.incrExpand( -1 )
                 }
-            },
-
-            'swipe-up': function ( ev ) {                
-                smk.$sidepanel.setExpand( 2 )
-            },
-
-            'swipe-down': function ( ev ) {
-                smk.$sidepanel.incrExpand( -1 )
-            }
-        } )
-
-        if ( !this.component )
-            this.content = { 
-                create: function ( el ) {
-                    SMK.HANDLER.get( self.id, 'activated' )( smk, self, el )
+            } )
+    
+            if ( !this.component )
+                this.content = { 
+                    create: function ( el ) {
+                        SMK.HANDLER.get( self.id, 'activated' )( smk, self, el )
+                    }
                 }
-            }
-
-        this.changedActive( function () {
-            if ( self.active ) {
-                if ( self.component )
-                    SMK.HANDLER.get( self.id, 'activated' )( smk, self )
-            }
-            else {
-                SMK.HANDLER.get( self.id, 'deactivated' )( smk, self )
-            }
-        } )
-
-        SMK.HANDLER.get( self.id, 'initialized' )( smk, self )
-    } )
-
-    return BespokeTool
+    
+            this.changedActive( function () {
+                if ( self.active ) {
+                    if ( self.component )
+                        SMK.HANDLER.get( self.id, 'activated' )( smk, self )
+                }
+                else {
+                    SMK.HANDLER.get( self.id, 'deactivated' )( smk, self )
+                }
+            } )
+    
+            SMK.HANDLER.get( self.id, 'initialized' )( smk, self )    
+        }
+    )
 } )
