6982b60cc4336c62c731ea0323bf2af0cd2f7b0b
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 14:25:18 -0700

    fix coordinate

--- src/smk/tool/coordinate/tool-coordinate.js
+++ src/smk/tool/coordinate/tool-coordinate.js
@@ -1,45 +1,33 @@
 include.module( 'tool-coordinate', [ 'tool.tool-js', 'tool-coordinate.coordinate-html' ], function ( inc ) {
     "use strict";
 
-    function CoordinateTool() {
-        SMK.TYPE.Tool.prototype.constructor.call( this )
-    }
+    return SMK.TYPE.Tool.define( 'CoordinateTool',
+        null,
+        function ( smk ) {
+            var self = this
 
-    SMK.TYPE.CoordinateTool = CoordinateTool
-
-    Object.assign( CoordinateTool.prototype, SMK.TYPE.Tool.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    SMK.TYPE.CoordinateTool.prototype.afterInitialize = SMK.TYPE.Tool.prototype.afterInitialize.concat( function ( smk ) {
-        var self = this
-
-        if ( smk.$device == 'mobile' ) return
-
-        this.model = {
-            latitude: null,
-            longitude: null,
-        }
-
-        this.vm = new Vue( {
-            el: smk.addToStatus( inc[ 'tool-coordinate.coordinate-html' ] ),
-            data: this.model,
-        } )
-
-        smk.$viewer.changedLocation( function ( ev ) {
-            if ( ev.map && ev.map.latitude ) {
-                self.model.latitude = ev.map.latitude
-                self.model.longitude = ev.map.longitude
+            if ( smk.$device == 'mobile' ) return
+    
+            this.model = {
+                latitude: null,
+                longitude: null,
             }
-            else {
-                self.model.latitude = null
-                self.model.longitude = null
-            }
-        } )
-    } )
-
-    return CoordinateTool
+    
+            this.vm = new Vue( {
+                el: smk.addToStatus( inc[ 'tool-coordinate.coordinate-html' ] ),
+                data: this.model,
+            } )
+    
+            smk.$viewer.changedLocation( function ( ev ) {
+                if ( ev.map && ev.map.latitude ) {
+                    self.model.latitude = ev.map.latitude
+                    self.model.longitude = ev.map.longitude
+                }
+                else {
+                    self.model.latitude = null
+                    self.model.longitude = null
+                }
+            } )    
+        } 
+    )
 } )
-
-
-
-
