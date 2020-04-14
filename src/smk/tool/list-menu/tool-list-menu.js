2620b54310b3930f2832fabc85e958107294f790
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 17:23:49 -0700

    fix list-menu

--- src/smk/tool/list-menu/tool-list-menu.js
+++ src/smk/tool/list-menu/tool-list-menu.js
@@ -1,4 +1,9 @@
-include.module( 'tool-list-menu', [ 'tool.tool-panel-js', 'tool-list-menu.panel-list-menu-html' ], function ( inc ) {
+include.module( 'tool-list-menu', [ 
+    'tool.tool-base-js', 
+    'tool.tool-widget-js', 
+    'tool.tool-panel-js', 
+    'tool-list-menu.panel-list-menu-html' 
+], function ( inc ) {
     "use strict";
 
     Vue.component( 'list-menu-widget', {
@@ -12,55 +17,45 @@ include.module( 'tool-list-menu', [ 'tool.tool-panel-js', 'tool-list-menu.panel-
     } )
     // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     //
-    function ListMenuTool() {
-        SMK.TYPE.ToolPanel.prototype.constructor.call( this, 'list-menu-panel', 'list-menu-widget' )
+    return SMK.TYPE.Tool.define( 'ListMenuTool', 
+        function () {
+            SMK.TYPE.ToolWidget.call( this, 'list-menu-widget' )
+            SMK.TYPE.ToolPanel.call( this, 'list-menu-panel' )
+        
+            this.defineProp( 'subWidgets' )
 
-        this.toolProp( 'subWidgets', { 
-            initial: [],
-            forWidget: false 
-        } )
-
-        this.icon = 'menu'
-        this.position = 'toolbar'
-    }
-
-    SMK.TYPE.ListMenuTool = ListMenuTool
-
-    $.extend( ListMenuTool.prototype, SMK.TYPE.ToolPanel.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    ListMenuTool.prototype.afterInitialize = SMK.TYPE.ToolPanel.prototype.afterInitialize.concat( function ( smk ) {
-        var self = this
-
-        smk.on( this.id, {
-            'swipe-up': function ( ev ) {                
-                smk.$sidepanel.setExpand( 2 )
-            },
-
-            'swipe-down': function ( ev ) {
-                smk.$sidepanel.incrExpand( -1 )
-            }
-        } )
-    } )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    ListMenuTool.prototype.addTool = function ( tool, smk, setParentId ) {
-        var self = this
-
-        if ( tool.parentId ) {
+            this.subWidgets = []
+            this.icon = 'menu'
+            this.position = 'toolbar'
+        },
+        function ( smk ) {
+            smk.on( this.id, {
+                'swipe-up': function ( ev ) {                
+                    smk.$sidepanel.setExpand( 2 )
+                },
+    
+                'swipe-down': function ( ev ) {
+                    smk.$sidepanel.incrExpand( -1 )
+                }
+            } )
+        },
+        {
+            addTool: function ( tool, smk, setParentId ) {
+                // if ( tool.makeWidgetComponent ) {          //  && !tool.parentId 
+                //     this.model.widgets.push( tool.makeWidgetComponent() )
+                // }
+                if ( !tool.parentId ) {
+                    setParentId( tool, this.id )
+                    this.subWidgets.push( tool.makeWidgetComponent() )
+                }
+        
+                smk.getSidepanel().addTool( tool, smk )
+        
+                tool.showTitle = true
+        
+                return true
+            }        
         }
-        else {
-            setParentId( tool, this.id )
-            this.subWidgets.push( tool.widget )
-        }
-
-        smk.getSidepanel().addTool( tool, smk )
-
-        tool.showTitle = true
-
-        return true
-    }
-
-    return ListMenuTool
+    )
 } )
 
