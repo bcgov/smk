d36903a9d0a5d23b275eb4bae654d8b61ea7370f
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 21:07:39 -0700

    fix minimap

--- src/smk/tool/minimap/tool-minimap.js
+++ src/smk/tool/minimap/tool-minimap.js
@@ -1,17 +1,6 @@
 include.module( 'tool-minimap', [ 'tool.tool-js' ], function () {
     "use strict";
 
-    function MinimapTool() {
-        SMK.TYPE.Tool.prototype.constructor.call( this )
-    }
-
-    SMK.TYPE.MinimapTool = MinimapTool
-
-    Object.assign( MinimapTool.prototype, SMK.TYPE.Tool.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    SMK.TYPE.MinimapTool.prototype.afterInitialize = SMK.TYPE.Tool.prototype.afterInitialize.concat()
-
-    return MinimapTool
+    return SMK.TYPE.Tool.define( 'MinimapTool' )
 } )
 
