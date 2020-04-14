5179cf7cb5a9a0d693e2fab9d30004555ef9244a
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 21:16:06 -0700

    fix shortcut-menu

--- src/smk/tool/shortcut-menu/tool-shortcut-menu.js
+++ src/smk/tool/shortcut-menu/tool-shortcut-menu.js
@@ -1,40 +1,31 @@
 include.module( 'tool-shortcut-menu', [ 'tool.tool-js', 'tool-shortcut-menu.shortcut-menu-html' ], function ( inc ) {
     "use strict";
 
-    function ShortcutMenuTool() {
-        SMK.TYPE.Tool.prototype.constructor.call( this )
-
-        this.model = {
-            widgets: []
-        }
-    }
-
-    SMK.TYPE.ShortcutMenuTool = ShortcutMenuTool
-
-    Object.assign( ShortcutMenuTool.prototype, SMK.TYPE.Tool.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    SMK.TYPE.ShortcutMenuTool.prototype.afterInitialize = SMK.TYPE.Tool.prototype.afterInitialize.concat( function ( smk ) {
-        this.vm = new Vue( {
-            el: smk.addToStatus( inc[ 'tool-shortcut-menu.shortcut-menu-html' ] ),
-            data: this.model,
-            methods: {
-                trigger: function ( toolId, event, arg, comp ) {
-                    smk.emit( toolId, event, arg, comp )
-                }
+    return SMK.TYPE.Tool.define( 'ShortcutMenuTool',
+        function () {
+            this.model = {
+                widgets: []
             }
-        } )
-    } )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    ShortcutMenuTool.prototype.addTool = function ( tool, smk ) {
-        smk.getSidepanel().addTool( tool, smk )
-
-        this.model.widgets.push( tool.widget )
-
-        return true
-    }
-
-    return ShortcutMenuTool
+        },
+        function ( smk ) {
+            this.vm = new Vue( {
+                el: smk.addToStatus( inc[ 'tool-shortcut-menu.shortcut-menu-html' ] ),
+                data: this.model,
+                methods: {
+                    trigger: function ( toolId, event, arg, comp ) {
+                        smk.emit( toolId, event, arg, comp )
+                    }
+                }
+            } )   
+        },
+        {
+            addTool: function ( tool, smk ) {
+                smk.getSidepanel().addTool( tool, smk )
+        
+                this.model.widgets.push( tool.makeWidgetComponent() )
+        
+                return true
+            }        
+        }
+    )
 } )
-
