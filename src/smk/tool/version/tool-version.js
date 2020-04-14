e6a8367bb9f15004cc8ed308eeb16af68ec59635
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 21:18:58 -0700

    fix version

--- src/smk/tool/version/tool-version.js
+++ src/smk/tool/version/tool-version.js
@@ -1,4 +1,9 @@
-include.module( 'tool-version', [ 'tool.tool-panel-js', 'tool-version.panel-version-html' ], function ( inc ) {
+include.module( 'tool-version', [ 
+    'tool.tool-base-js', 
+    'tool.tool-widget-js', 
+    'tool.tool-panel-js', 
+    'tool-version.panel-version-html' 
+], function ( inc ) {
     "use strict";
 
     Vue.component( 'version-widget', {
@@ -12,33 +17,24 @@ include.module( 'tool-version', [ 'tool.tool-panel-js', 'tool-version.panel-vers
     } )
     // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     //
-    function VersionTool() {
-        SMK.TYPE.ToolPanel.prototype.constructor.call( this, 'version-panel', 'version-widget' )
+    return SMK.TYPE.Tool.define( 'VersionTool', 
+        function () {
+            SMK.TYPE.ToolWidget.call( this, 'version-widget' )
+            SMK.TYPE.ToolPanel.call( this, 'version-panel' )
+        
+            this.defineProp( 'build' )
+            this.defineProp( 'config' )
 
-        this.toolProp( 'build', { 
-            initial: SMK.BUILD,
-            forWidget: false 
-        } )
-        this.toolProp( 'config', { 
-            forWidget: false 
-        } )
+            this.title = 'Version Info'
+            this.position = 'list-menu'
+            this.order = 99
+            this.icon = 'build'
+            this.build = SMK.BUILD
+        },
+        function ( smk ) {
+            this.config = SMK.UTIL.projection( 'lmfId', 'lmfRevision', 'createdBy', '_rev', 'published' )( smk )
 
-        this.title = 'Version Info'
-        this.position = 'list-menu'
-        this.order = 99
-        this.icon = 'build'
-    }
-
-    SMK.TYPE.VersionTool = VersionTool
-
-    $.extend( VersionTool.prototype, SMK.TYPE.ToolPanel.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    VersionTool.prototype.afterInitialize = SMK.TYPE.ToolPanel.prototype.afterInitialize.concat( function ( smk ) {
-        this.config = SMK.UTIL.projection( 'lmfId', 'lmfRevision', 'createdBy', '_rev', 'published' )( smk )
-
-        this.config.enabledTools = Object.keys( smk.$tool ).sort()
-    } )
-
-    return VersionTool
+            this.config.enabledTools = Object.keys( smk.$tool ).sort()
+        }
+    )
 } )
