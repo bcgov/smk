247298db3c83ecedc47d5fcb98cad9dd27309dff
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 21:10:52 -0700

    fix scale

--- src/smk/tool/scale/tool-scale.js
+++ src/smk/tool/scale/tool-scale.js
@@ -1,78 +1,76 @@
 include.module( 'tool-scale', [ 'tool.tool-js', 'tool-scale.scale-html' ], function ( inc ) {
     "use strict";
 
-    function ScaleTool() {
-        SMK.TYPE.Tool.prototype.constructor.call( this )
+    return SMK.TYPE.Tool.define( 'ScaleTool', 
+        function () {
+            SMK.TYPE.ToolWidget.call( this, 'baseMaps-widget' )
+            SMK.TYPE.ToolPanel.call( this, 'baseMaps-panel' )
+        
+            this.defineProp( 'showFactor' )
+            this.defineProp( 'showBar' )
 
-        this.showFactor = true
-        this.showBar = true
-    }
+            this.showFactor = true
+            this.showBar = true
+        },
+        function ( smk ) {
+            var self = this
 
-    SMK.TYPE.ScaleTool = ScaleTool
+            this.model = {
+                scaleDenom: null,
+                rulerSectionWidth: null,
+                rulerLength: null,
+                rulerUnit: null,
+                zoomLevel: null
+            }
 
-    Object.assign( ScaleTool.prototype, SMK.TYPE.Tool.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    SMK.TYPE.ScaleTool.prototype.afterInitialize = SMK.TYPE.Tool.prototype.afterInitialize.concat( function ( smk ) {
-        var self = this
+            this.vm = new Vue( {
+                el: smk.addToStatus( inc[ 'tool-scale.scale-html' ] ),
+                data: this.model,
+            } )
 
-        this.model = {
-            scaleDenom: null,
-            rulerSectionWidth: null,
-            rulerLength: null,
-            rulerUnit: null,
-            zoomLevel: null
-        }
+            smk.$viewer.changedView( function ( ev ) {
+                self.refresh()
+            } )
 
-        this.vm = new Vue( {
-            el: smk.addToStatus( inc[ 'tool-scale.scale-html' ] ),
-            data: this.model,
-        } )
+            this.refresh = function () {
+                this.model.scaleDenom = null
+                this.model.rulerSectionWidth = null
+                this.model.rulerLength = null
 
-        smk.$viewer.changedView( function ( ev ) {
+                var view = smk.$viewer.getView()
+                if ( !view ) return
+
+                if ( this.showFactor !== false && view.scale ) {
+                    this.model.scaleDenom = view.scale
+                    this.model.zoomLevel = view.zoom
+                }
+
+                if ( this.showBar !== false && view.metersPerPixel ) {
+                    var rulerMM = rounded( 200 * view.metersPerPixel * 1000 )
+                    this.model.rulerSectionWidth = rulerMM / 1000 / view.metersPerPixel / 4
+
+                    var dist = appropriateUnit( rulerMM )
+                    this.model.rulerLength = dist.value
+                    this.model.rulerUnit = dist.unit
+                }
+            }
+
+            var firstDigit = [ null, 1, 2, 3, 5, 5, 5, 5, 10, 10 ]
+
+            function rounded( s ) {
+                var f = firstDigit[ 1 * ( s + '' )[ 0 ] ]
+
+                return f * Math.pow( 10, ( Math.floor( s ) + '' ).length - 1 )
+            }
+
+            function appropriateUnit( mm ) {
+                if ( mm <= 500 * 1000 ) return { value: mm / 1000, unit: 'm' }
+                return { value: mm / 1000 / 1000, unit: 'km' }
+            }
+
             self.refresh()
-        } )
-
-        this.refresh = function () {
-            this.model.scaleDenom = null
-            this.model.rulerSectionWidth = null
-            this.model.rulerLength = null
-
-            var view = smk.$viewer.getView()
-            if ( !view ) return
-
-            if ( this.showFactor !== false && view.scale ) {
-                this.model.scaleDenom = view.scale
-                this.model.zoomLevel = view.zoom
-            }
-
-            if ( this.showBar !== false && view.metersPerPixel ) {
-                var rulerMM = rounded( 200 * view.metersPerPixel * 1000 )
-                this.model.rulerSectionWidth = rulerMM / 1000 / view.metersPerPixel / 4
-
-                var dist = appropriateUnit( rulerMM )
-                this.model.rulerLength = dist.value
-                this.model.rulerUnit = dist.unit
-            }
-        }
-
-        var firstDigit = [ null, 1, 2, 3, 5, 5, 5, 5, 10, 10 ]
-
-        function rounded( s ) {
-            var f = firstDigit[ 1 * ( s + '' )[ 0 ] ]
-
-            return f * Math.pow( 10, ( Math.floor( s ) + '' ).length - 1 )
-        }
-
-        function appropriateUnit( mm ) {
-            if ( mm <= 500 * 1000 ) return { value: mm / 1000, unit: 'm' }
-            return { value: mm / 1000 / 1000, unit: 'km' }
-        }
-
-        self.refresh()
-    } )
-
-    return ScaleTool
+        } 
+    )
 } )
 
 
