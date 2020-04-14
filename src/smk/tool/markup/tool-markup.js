f01aecbc400dd1c1768d3fcf349eb4ab21430fa2
Benjamin Jubb <benjubb@gmail.com>
2020-04-13 20:48:54 -0700

    fix markup

--- src/smk/tool/markup/tool-markup.js
+++ src/smk/tool/markup/tool-markup.js
@@ -1,17 +1,5 @@
 include.module( 'tool-markup', [ 'tool.tool-js' ], function () {
     "use strict";
 
-    function MarkupTool() {
-        SMK.TYPE.Tool.prototype.constructor.call( this )
-    }
-
-    SMK.TYPE.MarkupTool = MarkupTool
-
-    Object.assign( MarkupTool.prototype, SMK.TYPE.Tool.prototype )
-    // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
-    //
-    MarkupTool.prototype.afterInitialize = SMK.TYPE.Tool.prototype.afterInitialize.concat()
-
-    return MarkupTool
+    return SMK.TYPE.Tool.define( 'MarkupTool' )
 } )
-
