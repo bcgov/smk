b38cf4789381104e6187b0d069c7699eff9b6a4c
Benjamin Jubb <benjubb@gmail.com>
2020-04-18 13:03:47 -0700

    cleanup

--- src/smk/viewer-leaflet/feature-list-clustering-leaflet.js
+++ src/smk/viewer-leaflet/feature-list-clustering-leaflet.js
@@ -5,7 +5,7 @@ include.module( 'feature-list-clustering-leaflet', [
 ], function ( inc ) {
     "use strict";
 
-    var base = include.option( 'baseUrl' ) + 'images/feature-list'
+    var base = include.option( 'baseUrl' ) + 'images/viewer-leaflet'
 
     var whiteMarker = new L.Icon( {
         iconUrl:        base + '/marker-icon-white.png',
