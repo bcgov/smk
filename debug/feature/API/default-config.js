5912d09d214a335cb18c966918ef5ddbf9b772f1
Benjamin Jubb <benjubb@gmail.com>
2020-07-15 11:46:45 -0700

    add debug feature for showFeature API

---
+++ debug/feature/API/default-config.js
@@ -0,0 +1,35 @@
+var defaultConfig = [
+    './../../config/victoria.json',
+    {
+        "tools": [
+            {
+                "type": "toolbar",
+                "enabled": true
+            },
+            {
+                "type": "zoom",
+                "enabled": true
+            },
+            {
+                "type": "pan",
+                "enabled": true
+            },
+            {
+                "type": "markup",
+                "enabled": false
+            },
+            {
+                "type": "search",
+                "enabled": false
+            },
+            {
+                "type": "location",
+                "enabled": false
+            },
+            {
+                "type": "layers",
+                "enabled": false
+            }
+        ]
+    }
+]
