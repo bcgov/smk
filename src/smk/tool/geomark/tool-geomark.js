d2dc8a9ef577365d9cbe0f3f9c541a7f27cec98f
Michael Nelson <michael.nelson@gov.bc.ca>
2021-10-12 13:29:23 -0700

    Can create a Geomark (#129)
    * Can create a Geomark
    
    This adds support for creating a Geomark from polygon coordinates in WKT format using the Geomark web service.
    
    * Updates from review comments

--- src/smk/tool/geomark/tool-geomark.js
+++ src/smk/tool/geomark/tool-geomark.js
@@ -1,4 +1,5 @@
 include.module( 'tool-geomark', [ 
+    'geomark',
     'tool.tool-base-js',
     'tool.tool-widget-js',
     'tool.tool-panel-js',
@@ -20,9 +21,38 @@ include.module( 'tool-geomark', [
         function () {
             SMK.TYPE.ToolWidget.call( this, 'geomark-widget' )
             SMK.TYPE.ToolPanel.call( this, 'geomark-panel' )
+
+            this.defineProp( 'geomarkService' )
         },
         function ( smk ) {
             var self = this
+
+            if (!self.geomarkService) {
+                self.showStatusMessage('No value for "geomarkService" was found in configuration. Geomark tool functionality is disabled.', 'error', 5000);
+                return;
+            }
+
+            this.buildLngLatCoords = function(layerGroup) {
+                var lngLatCoords = '';
+                layerGroup.getLayers().forEach(function(layer, index, array) {
+                    layer.getLatLngs().forEach(function(pointArray) {
+                        var firstPointStr = '';
+                        lngLatCoords += '(';
+                        pointArray.forEach(function(point, index, array){
+                            var lngLatCoord = point.lng + ' ' + point.lat;
+                            if (index == 0) {
+                                firstPointStr = lngLatCoord;
+                            }
+                            lngLatCoords += lngLatCoord + ', ';
+                        });
+                        lngLatCoords += firstPointStr + ')'; // close the polygon
+                        if (index != (array.length - 1)) {
+                            lngLatCoords += ', ';
+                        }
+                    });
+                });
+                return lngLatCoords;
+            }
 
             this.changedActive( function () {
                 if ( self.active ) {
@@ -44,6 +74,31 @@ include.module( 'tool-geomark', [
 
                 geomarkLayers.addLayer(layer);
             });
+
+            var client = new window.GeomarkClient(self.geomarkService.url);
+
+            smk.on( this.id, {
+                'create-geomark': function () {
+                    if (geomarkLayers.getLayers().length == 0) {
+                        self.showStatusMessage('No drawings were found. Draw one or more polygons before creating a geomark.', 'warning');
+                        return;
+                    }
+                    var lngLatCoords = self.buildLngLatCoords(geomarkLayers);
+                    client.createGeomark({
+                        'body': 'SRID=4326;POLYGON(' + lngLatCoords + ')',
+                        'format': 'wkt',
+                        'callback': function(geomarkInfo) {
+                            var geomarkId = geomarkInfo.id;
+                            if (geomarkId) { 
+                                alert('Created geomark: ' + geomarkInfo.url + 
+                                '. Save this URL to access your geomark later.');
+                            } else {
+                                self.showStatusMessage('Error creating geomark: ' + geomarkInfo.error, 'error', 5000);
+                            }
+                        }
+                    });
+                }
+            })
         }
     )
 } )
