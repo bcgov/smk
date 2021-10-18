7a4eaa253ac6d2e272a6ee98a9cbbcb0c773279e
Michael Nelson <michael.nelson@gov.bc.ca>
2021-10-18 12:00:27 -0700

    Geomarks are listed (#131)
    Geomarks are added to a list upon creation. Geomarks in the list have a checkbox that toggles visibility. Geomarks are hyperlinked.

--- src/smk/tool/geomark/tool-geomark.js
+++ src/smk/tool/geomark/tool-geomark.js
@@ -9,27 +9,37 @@ include.module( 'tool-geomark', [
 
     Vue.component( 'geomark-widget', {
         extends: SMK.COMPONENT.ToolWidgetBase,
-    } )
+    } );
 
     Vue.component( 'geomark-panel', {
         extends: SMK.COMPONENT.ToolPanelBase,
-        template: inc[ 'tool-geomark.panel-geomark-html' ]
-    } )
+        template: inc[ 'tool-geomark.panel-geomark-html' ],
+        props: [ 'geomarks' ]
+    } );
     // _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
     //
     return SMK.TYPE.Tool.define( 'GeomarkTool',
         function () {
-            SMK.TYPE.ToolWidget.call( this, 'geomark-widget' )
-            SMK.TYPE.ToolPanel.call( this, 'geomark-panel' )
+            SMK.TYPE.ToolWidget.call( this, 'geomark-widget' );
+            SMK.TYPE.ToolPanel.call( this, 'geomark-panel' );
 
-            this.defineProp( 'geomarkService' )
+            this.defineProp( 'geomarkService' );
+            this.defineProp( 'geomarks' );
+
+            this.geomarks = [];
         },
         function ( smk ) {
-            var self = this
+            var self = this;
 
             if (!self.geomarkService) {
                 self.showStatusMessage('No value for "geomarkService" was found in configuration. Geomark tool functionality is disabled.', 'error', 5000);
                 return;
+            }
+
+            this.createCurrentDrawingLayer = function() {
+                var drawingLayer = new L.FeatureGroup();
+                smk.$viewer.map.addLayer(drawingLayer);
+                return drawingLayer;
             }
 
             this.buildLngLatCoords = function(layerGroup) {
@@ -38,7 +48,7 @@ include.module( 'tool-geomark', [
                     layer.getLatLngs().forEach(function(pointArray) {
                         var firstPointStr = '';
                         lngLatCoords += '(';
-                        pointArray.forEach(function(point, index, array){
+                        pointArray.forEach(function(point, index) {
                             var lngLatCoord = point.lng + ' ' + point.lat;
                             if (index == 0) {
                                 firstPointStr = lngLatCoord;
@@ -65,25 +75,35 @@ include.module( 'tool-geomark', [
                 }
             } )
 
-            var geomarkLayers = new L.FeatureGroup();
-            smk.$viewer.map.addLayer(geomarkLayers);
+            var currentDrawingLayer = self.createCurrentDrawingLayer();
+
+            this.toGeomark = function(geomarkInfo, drawingLayer) {
+                return {
+                    id: geomarkInfo.id,
+                    url: geomarkInfo.url,
+                    drawingLayer: drawingLayer
+                };
+            }
+
+            this.getGeomarkById = function(geomarkId) {
+                return self.geomarks.find(function(item) {
+                    return item.id === geomarkId;
+                });
+            }
 
             smk.$viewer.map.on('pm:create', function(e) {
-                var type = e.layerType;
-                var layer = e.layer;
-
-                geomarkLayers.addLayer(layer);
+                currentDrawingLayer.addLayer(e.layer);
             });
 
             var client = new window.GeomarkClient(self.geomarkService.url);
 
             smk.on( this.id, {
                 'create-geomark': function () {
-                    if (geomarkLayers.getLayers().length == 0) {
-                        self.showStatusMessage('No drawings were found. Draw one or more polygons before creating a geomark.', 'warning');
+                    if (currentDrawingLayer.getLayers().length == 0) {
+                        self.showStatusMessage('No drawings were found. Draw one or more polygons before creating a geomark.', 'warning', 5000);
                         return;
                     }
-                    var lngLatCoords = self.buildLngLatCoords(geomarkLayers);
+                    var lngLatCoords = self.buildLngLatCoords(currentDrawingLayer);
                     client.createGeomark({
                         'body': 'SRID=4326;POLYGON(' + lngLatCoords + ')',
                         'format': 'wkt',
@@ -92,11 +112,24 @@ include.module( 'tool-geomark', [
                             if (geomarkId) { 
                                 alert('Created geomark: ' + geomarkInfo.url + 
                                 '. Save this URL to access your geomark later.');
+                                self.geomarks.push(self.toGeomark(geomarkInfo, currentDrawingLayer));
+                                currentDrawingLayer = self.createCurrentDrawingLayer();
                             } else {
                                 self.showStatusMessage('Error creating geomark: ' + geomarkInfo.error, 'error', 5000);
                             }
                         }
                     });
+                },
+                'toggle-geomark': function(idObj) {
+                    var geomark = self.getGeomarkById(idObj.id);
+                    if (!geomark) {
+                        return;
+                    }
+                    if (smk.$viewer.map.hasLayer(geomark.drawingLayer)) {
+                        smk.$viewer.map.removeLayer(geomark.drawingLayer);
+                    } else {
+                        smk.$viewer.map.addLayer(geomark.drawingLayer);
+                    }
                 }
             })
         }
