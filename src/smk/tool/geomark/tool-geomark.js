a434e11f71ac1e21d051ea5bd038211c553becaa
Michael Nelson <michael.nelson@gov.bc.ca>
2021-10-20 08:30:34 -0700

    Can load a geomark from a URL (#132)

--- src/smk/tool/geomark/tool-geomark.js
+++ src/smk/tool/geomark/tool-geomark.js
@@ -31,6 +31,10 @@ include.module( 'tool-geomark', [
         function ( smk ) {
             var self = this;
 
+            // Check for "geomarkService" configuration. Example:
+            // "geomarkService": {
+            //     "url": "https://apps.gov.bc.ca/pub/geomark"
+            // }
             if (!self.geomarkService) {
                 self.showStatusMessage('No value for "geomarkService" was found in configuration. Geomark tool functionality is disabled.', 'error', 5000);
                 return;
@@ -42,27 +46,50 @@ include.module( 'tool-geomark', [
                 return drawingLayer;
             }
 
-            this.buildLngLatCoords = function(layerGroup) {
-                var lngLatCoords = '';
-                layerGroup.getLayers().forEach(function(layer, index, array) {
+            this.buildWktCoords = function(layerGroup) {
+                var isMultiPolygon = layerGroup.getLayers().length > 1;
+                var wktCoords = isMultiPolygon ? "MULTIPOLYGON(" : "POLYGON(";
+                var openCoords = isMultiPolygon ? '((' : '(';
+                var closeCoords = isMultiPolygon ? '))' : ')';
+                layerGroup.getLayers().forEach(function(layer, layerIndex, layerArray) {
                     layer.getLatLngs().forEach(function(pointArray) {
                         var firstPointStr = '';
-                        lngLatCoords += '(';
-                        pointArray.forEach(function(point, index) {
+                        wktCoords += openCoords;
+                        pointArray.forEach(function(point, pointIndex){
                             var lngLatCoord = point.lng + ' ' + point.lat;
-                            if (index == 0) {
+                            if (pointIndex == 0) {
                                 firstPointStr = lngLatCoord;
                             }
-                            lngLatCoords += lngLatCoord + ', ';
+                            wktCoords += lngLatCoord + ', ';
                         });
-                        lngLatCoords += firstPointStr + ')'; // close the polygon
-                        if (index != (array.length - 1)) {
-                            lngLatCoords += ', ';
+                        wktCoords += firstPointStr + closeCoords; // close the polygon
+                        if (layerIndex != (layerArray.length - 1)) {
+                            wktCoords += ', ';
                         }
                     });
                 });
-                return lngLatCoords;
+                return wktCoords + ')';
             }
+
+            this.tidyUrl = function(url) {
+                url = url.trim();
+                if (url.endsWith('/')) {
+                    url = url.substring(0, (url.length - 1));
+                }
+                url = url.split('?')[0];
+                url = encodeURI(url);
+                return url;
+            }
+
+            this.extractGeomarkId = function(geomarkUrl){
+                if (!geomarkUrl) {
+                    return;
+                }
+                var lastSlashIndex = geomarkUrl.lastIndexOf('/');
+                if (lastSlashIndex > 0) {
+                    return geomarkUrl.substring(lastSlashIndex + 1);
+                }
+            } 
 
             this.changedActive( function () {
                 if ( self.active ) {
@@ -79,8 +106,8 @@ include.module( 'tool-geomark', [
 
             this.toGeomark = function(geomarkInfo, drawingLayer) {
                 return {
-                    id: geomarkInfo.id,
-                    url: geomarkInfo.url,
+                    id: geomarkInfo.id || geomarkInfo.properties.id,
+                    url: geomarkInfo.url || geomarkInfo.properties.url,
                     drawingLayer: drawingLayer
                 };
             }
@@ -103,9 +130,9 @@ include.module( 'tool-geomark', [
                         self.showStatusMessage('No drawings were found. Draw one or more polygons before creating a geomark.', 'warning', 5000);
                         return;
                     }
-                    var lngLatCoords = self.buildLngLatCoords(currentDrawingLayer);
+                    var wktCoords = self.buildWktCoords(currentDrawingLayer);
                     client.createGeomark({
-                        'body': 'SRID=4326;POLYGON(' + lngLatCoords + ')',
+                        'body': 'SRID=4326;' + wktCoords,
                         'format': 'wkt',
                         'callback': function(geomarkInfo) {
                             var geomarkId = geomarkInfo.id;
@@ -117,6 +144,31 @@ include.module( 'tool-geomark', [
                             } else {
                                 self.showStatusMessage('Error creating geomark: ' + geomarkInfo.error, 'error', 5000);
                             }
+                        }
+                    });
+                },
+                'load-geomark': function() {
+                    var enteredUrl = prompt('Enter the URL of a geomark to load:');
+                    var geomarkUrl = self.tidyUrl(enteredUrl);
+                    var geomarkId = self.extractGeomarkId(geomarkUrl);
+                    if (!geomarkId) {
+                        self.showStatusMessage('Could not discern a geomark ID within "' + geomarkUrl + '"', 'warning', 5000);
+                        return;
+                    }
+                    if (self.getGeomarkById(geomarkId)) {
+                        self.showStatusMessage('Geomark ' + geomarkId + ' is already loaded.', 'warning', 5000);
+                        return;
+                    }
+                    $.ajax({
+                        url: geomarkUrl + '/feature.geojson',
+                        dataType: 'json',
+                        traditional: true,
+                        success: function(geomarkFeature) {
+                            var geometryLayer = L.geoJSON(geomarkFeature.geometry).addTo(smk.$viewer.map);
+                            self.geomarks.push(self.toGeomark(geomarkFeature, geometryLayer));
+                        },
+                        error: function(jqXHR, textStatus, errorThrown) {
+                            self.showStatusMessage('Error retrieving geomark from URL ' + geomarkUrl + ': ' + errorThrown, 'error', 5000);
                         }
                     });
                 },
